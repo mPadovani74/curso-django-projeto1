2587f2103b26b45fbadf0fbffefab52a7da0f555
mPadovani <matheuspereira35@hotmail.com>
2024-08-07 14:51:28 -0300

    templates e primeiras views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,13 +1,10 @@
 
 from django.urls import path, include
-from django.contrib import admin
-from recipes.views import home, contato, sobre
+
+from recipes.views import home
 
 
 # dominio/recipes/
 urlpatterns = [
-    path('sobre/', sobre), # /sobre/
-    path('home/', sobre), # /sobre/
     path('', home), # Home
-    path('contato/', contato), # /contato/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name':'nome',
     })
-
-def contato(request):
-    return render(request, 'temp.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
